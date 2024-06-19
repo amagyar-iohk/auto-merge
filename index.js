7dc16009255b3c08d6235af358f7dcc141ea99c2
Allain Magyar <allain.magyar@iohk.io>
2024-06-19 16:43:21 -0300

    add colors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gh = require('@actions/github');
 const core = require('@actions/core');
+const colors = require('colors')
 
 async function run() {
     const token = process.env.GITHUB_TOKEN
@@ -23,47 +24,47 @@ async function run() {
     const labeledPullRequests = pullRequests.filter(pr => pr.labels.filter(label => label.name == 'autoupdate').length > 0)
 
     for (let pr of labeledPullRequests) {
-        console.info("Updating pull request:", pr.title, "-", pr.issue_url)
-
-        console.log("head", pr.head.ref)
-        console.info("base", pr.base.ref)
-
+        console.info("Check pull request number [".blue + `${pr.number}`.yellow + "]".blue)
+        console.info("  -", "title:".blue, pr.title)
+        console.info("  -", "url:".blue, pr.html_url)
         try {
-            let result = await github.rest.repos.compareCommits({
+            let changes = await github.rest.repos.compareCommits({
                 ...context.repo,
                 base: pr.head.ref,
                 head: pr.base.ref,
-                mediaType: {
-                    format: "diff"
-                }
+                // mediaType: {
+                //     format: "diff"
+                // }
             });
 
-            console.log(result)
 
-            //  await github.rest.pulls.updateBranch({
-            //     ...context.repo,
-            //     expected_head_sha: pr.head.sha,
-            //     pull_number: pr.number,
-            // });
+            // after merging main to PR the comparison becomes 'behind'
+            if (changes.data.status == 'behind') {
+                console.info("  -", "comparison to main:".blue, "no changes detected".green)
+                console.info()
+                continue
+            }
 
-            // console.log(result)
+            console.info("  -", "comparison to main:".blue, "changes detected".red)
 
-            // if (result.status == 202) {
-            //     await github.rest.issues.createComment({
-            //         ...context.repo,
-            //         issue_number: pr.number,
-            //         body: 'Merge success'
-            //     });
-            // } else {
-            //     await github.rest.issues.createComment({
-            //         ...context.repo,
-            //         issue_number: pr.number,
-            //         body: 'Merge failed'
-            //     });
-            // }
+            // merge main to pull request
+            let updateResult = await github.rest.pulls.updateBranch({
+                ...context.repo,
+                expected_head_sha: pr.head.sha,
+                pull_number: pr.number,
+            });
 
+            // comment on pull request
+            let commentMessage = updateResult.status == 202 ? "Merge success" : "Merge failed"
+            let commentResult = await github.rest.issues.createComment({
+                ...context.repo,
+                issue_number: pr.number,
+                body: commentMessage
+            });
+            console.info("  - comment created", commentResult.status == 201 ? "successfully" : "unsuccessfully")
+            console.info()
         } catch (err) {
-            core.info(err);
+            core.error(err);
         }
     }
     // until here
